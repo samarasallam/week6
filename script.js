776b941c6ff250c740f9f11077df3f4659bee76f
samarasallam <39006846+samarasallam@users.noreply.github.com>
2018-07-23 20:08:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 console.log("Homework week 6");
 
-$(document).ready function (){
-    $('#btnClick').click(function() {})
+$(document).ready (function (){
+    $('#btnClick').click(function() {
 	let resultElements = $(#hyfResults);
-
+			       
 	$.ajax( {
-
             url: 'https://api.github.com/orgs/HackYourFuture/repos',
             method: 'get',
             dataType: 'json',
