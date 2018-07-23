e8793cc844ac25c4b8644699291be2f31506ed7a
samarasallam <39006846+samarasallam@users.noreply.github.com>
2018-07-23 17:40:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 console.log("Homework week 6");
 
 $(document).ready function (){
-$('#btnClick').click (function () {
+    $('#btnClick').click(function() {})
 	let resultElements = $(#hyfResults);
 
-	$.Ajax( {
+	$.ajax( {
 
-		url: 'https://api.github.com/orgs/HackYourFuture/repos';
-		method: 'get';
-		dataType: 'json';
-		success : function (data) {
-			resultElements.html('HYF data' + data)
-		}
-	})
-}
-}
+            url: 'https://api.github.com/orgs/HackYourFuture/repos',
+            method: 'get',
+            dataType: 'json',
+            success: function(data) {
+                resultElements.html('HYF data' + data)
+            }
+        });
+    });
+});
+
+
